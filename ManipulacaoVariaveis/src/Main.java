fa724b7865a0e1be1e0f79fabbae9edd77482099
eduardocolella <eduardo310301@edu.unipar.br>
2024-08-19 16:19:09 -0300

    Add files via upload

---
+++ ManipulacaoVariaveis/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Variaveis");
+
+    // Criação de variáveis //
+    String nome = "Eduardo";
+    int idade = 23;
+    double altura = 1.98;
+    boolean estudante = true;
+
+    // operação simples //
+    int idadeIrmao = 20;
+    int somaIdades = idade + idadeIrmao;
+
+    double alturaDuplicada = altura * 2;
+    String saudacao = "Oi, meu nome é " + nome + "!";
+
+    // exibição de resultados//
+    System.out.println("Idade" + idade);
+    System.out.println("Altura: " + altura);
+    System.out.println("Nome: " + nome);
+    System.out.println("É estudante? " + estudante);
+    System.out.println("Soma das idades: " + somaIdades);
+    System.out.println("Altura multiplicada por 2: " + alturaDuplicada);
+    System.out.println(saudacao);
+
+
+        }
+    }
